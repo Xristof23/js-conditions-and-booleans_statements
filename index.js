853bff1c5f5645cf14ba738b9ad71f4457c856cb
Christof Röhl <userpattern@gmail.com>
2023-12-18 11:36:19 +0100

    solved

--- index.js
+++ index.js
@@ -3,24 +3,44 @@ console.clear();
 // Part 1: Password
 const SUPER_SECRET_PASSWORD = "h4x0r1337";
 
-const receivedPassword = "password1234";
+const receivedPassword = "h4x0r1337";
+
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
 
 // Part 2: Even / Odd
-const number = 6;
+const number = 7;
 
+if (number % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
 // Part 3: Hotdogs
-const numberOfHotdogs = 42;
-
+const numberOfHotdogs = 3000000;
+console.clear;
+if (numberOfHotdogs < 5) {
+  console.log("Hotdog price: " + "2 € per hotdog");
+} else if (numberOfHotdogs < 100) {
+  console.log("Hotdog price: " + "1,50 € per hotdog");
+} else if (numberOfHotdogs < 1000000) {
+  console.log("Hotdog price: " + "1 € per hotdog");
+} else {
+  console.log("Hotdog price: " + "0,10 € per hotdog");
+}
 // Part 4: Daytime
-const currentHour = 12;
+const currentHour = 11.5;
 
-const statement = "";
+const statement = currentHour < 13 ? "Still need to learn ..." : "Partytime!!!";
 
 console.log(statement);
 
 // Part 5: Greeting
-const name = "Archibald";
+const name = "Feline";
 
-const greeting = "Hello " + "//enter your code here" + "!";
+const greeting = "Hello " + (name === "Feline" ? "Coach" : name) + "!";
 
 console.log(greeting);
